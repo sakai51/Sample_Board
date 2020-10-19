08c735fd977b647a88e6d1c537ead4c8a251d345
Takashi Sakai <stakashi51@gmail.com>
2020-10-19 23:41:17 +0900

    Initial commit

---
+++ app/Comment.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'post_id', 'comment'
+    ];
+
+    public function user(){
+        // 投稿は1つのカテゴリーに属する
+        return $this->belongsTo(\App\User::class,'user_id');
+    }
+
+}
